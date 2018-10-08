62880f036c6809c84fe144f4379b2e32dfef0417
Jarvis Schultz <schultzjarvis@gmail.com>
2018-10-08 08:11:57 -0500

    Finished script, cleaned CMakeLists, and added launch file

--- src/show_file_directory.py
+++ src/show_file_directory.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import os
 
+OKBLUE = '\033[94m'
+ENDC = '\033[0m'
+
 def main():
     # all we want to do in here is show the location of the executable:
+    print ""
+    print OKBLUE+"================================================================================"
     print "Path of the executable itself is = ", os.path.abspath(__file__)
+    print "================================================================================"+ENDC
+    print ""
     return
 
 if __name__ == '__main__':
